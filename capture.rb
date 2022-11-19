5103b9ed7e44bdba1c84592e9860fd2ff31c600e
fahmiin <fahmiinabdullah96@gmail.com>
2022-11-20 00:48:01 +0800

    feat: added rudimentary error handling

--- capture.rb
+++ capture.rb
@@ -2,17 +2,31 @@ require 'net/http'
 require 'nokogiri'
 
 class Autifyweb
-  def initialize(uri)
-    @uri = uri
+  def initialize(url)
+    @url = url
   end
 
   def parse
-    source = Net::HTTP.get(@uri)
+    check_url
+
+    source = Net::HTTP.get(@url)
     contents = Nokogiri::HTML(source)
-    path = File.basename(@uri.to_s)
+    path = File.basename(@url.to_s)
     path += '.html' if path !~ /\.((html?)|(txt))$/
 
     File.open(path, 'w') { |f| f.write(contents.to_html) }
+  end
+
+  def check_url
+    response = Net::HTTP.get_response(@url)
+    if response.code.to_i >= 400
+      $stderr.puts "Process exited with ERROR #{response.code}: #{@url}"
+      return
+    end
+
+    if %w[301 302 307].include?(response.code)
+      @url = URI.parse(response['location'])
+    end
   end
 end
 
