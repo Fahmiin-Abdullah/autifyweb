1b3e5e0180934fdc39c5b6ecf5ec8968ab84bd34
fahmiin <fahmiinabdullah96@gmail.com>
2022-11-20 01:22:58 +0800

    feat: use optparse and added link counter

--- capture.rb
+++ capture.rb
@@ -1,9 +1,11 @@
 require 'net/http'
 require 'nokogiri'
+require 'optparse'
 
 class Autifyweb
-  def initialize(url)
+  def initialize(url, with_metadata = false)
     @url = url
+    @with_metadata = with_metadata
   end
 
   def parse
@@ -15,6 +17,9 @@ class Autifyweb
     path += '.html' if path !~ /\.((html?)|(txt))$/
 
     File.open(path, 'w') { |f| f.write(contents.to_html) }
+    return unless @with_metadata
+
+    print_metadata(contents)
   end
 
   def check_url
@@ -28,16 +33,36 @@ class Autifyweb
       @url = URI.parse(response['location'])
     end
   end
+
+  def print_metadata(contents)
+    links_count = contents.xpath('//a[@href]').count
+
+    p "Number of links: #{links_count}"
+  end
 end
+
+OptionParser.new do |parser|
+  parser.banner = "Usage: capture.rb [options]"
+
+  parser.on("-m", "--metadata", "Fetch the metadata") do
+    @metadata_flag = true
+  end
+
+  parser.on("-h", "--help", "Show this help message") do
+    puts parser
+  end
+end.parse!
 
 if __FILE__ == $0
   if ARGV.count < 1
-    $stderr.puts "Please enter at least 1 URL link"
+    $stderr.puts 'Please enter at least 1 URL link'
     exit 1
   end
 
   ARGV.each do |arg|
-    doc = Autifyweb.new(URI.parse(arg))
+    doc = Autifyweb.new(URI.parse(arg), @metadata_flag)
     doc.parse
   end
+
+  puts 'Process completed!'
 end
