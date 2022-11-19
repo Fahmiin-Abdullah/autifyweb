c2f54afe2b8f8b6d0fd19a7e9d79688c2101b8c2
fahmiin <fahmiinabdullah96@gmail.com>
2022-11-20 03:13:16 +0800

    feat: allow for read metadata or write html only

--- capture.rb
+++ capture.rb
@@ -4,28 +4,24 @@ require 'nokogiri'
 require 'optparse'
 
 class Autifyweb
-  def initialize(url, with_metadata = false)
+  def initialize(url)
     @url = url
-    @with_metadata = with_metadata
-    @dir = generate_dir_name
   end
 
   def parse
     check_url
 
-    Dir.mkdir(@dir) if (! File.exist? @dir)
+    dir = generate_dir_name
+    Dir.mkdir(dir) if (! File.exist? dir)
     source = Net::HTTP.get(@url)
     contents = Nokogiri::HTML(source)
 
-    html_path = File.join(@dir, File.basename(@url.to_s))
+    html_path = File.join(dir, File.basename(@url.to_s))
     html_path += '.html' if html_path !~ /\.((html?)|(txt))$/
-    meta_path = File.join(@dir, 'metadata')
+    meta_path = File.join(dir, 'metadata')
 
     File.open(html_path, 'w') { |f| f.write(contents.to_html) }
     File.open(meta_path, 'w') { |f| f.write(fetch_metadata(contents)) }
-    return unless @with_metadata
-
-    puts File.read(meta_path)
   end
 
   def check_url
@@ -50,7 +46,14 @@ class Autifyweb
     links_count = contents.xpath('//a[@href]').count
     images_count = contents.xpath('//img[@src]').count
 
-    "Site name: #{@url}#{$/}Number of links: #{links_count}#{$/}Number of images: #{images_count}#{$/}Last fetched: #{DateTime.now.strftime('%m/%d/%Y %I:%M %p')}"
+    "#{$/}Site name: #{@url}#{$/}Number of links: #{links_count}#{$/}Number of images: #{images_count}#{$/}Last fetched: #{DateTime.now.strftime('%m/%d/%Y %I:%M %p')}"
+  end
+
+  def print_metadata(dir)
+    puts File.read(File.join(dir, 'metadata'))
+  rescue Errno::ENOENT
+    $stderr.puts "The folder for #{dir} could not be found!"
+    exit 1
   end
 end
 
@@ -68,13 +71,18 @@ end.parse!
 
 if __FILE__ == $0
   if ARGV.count < 1
-    $stderr.puts 'Please enter at least 1 URL link'
+    $stderr.puts 'Please enter at least 1 URL link or directory name'
     exit 1
   end
 
   ARGV.each do |arg|
-    doc = Autifyweb.new(URI.parse(arg), @metadata_flag)
-    doc.parse
+    doc = Autifyweb.new(URI.parse(arg))
+
+    if @metadata_flag
+      doc.print_metadata(arg)
+    else
+      doc.parse
+    end
   end
 
   puts 'Process completed!'
